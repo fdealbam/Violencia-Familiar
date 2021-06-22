928ad4756a8a14c6ff14e9978eb01689ab62956d
Felipe de Alba <45267327+fdealbam@users.noreply.github.com>
2021-06-22 12:55:40 -0500

    Update dash.py

--- application/dash.py
+++ application/dash.py
@@ -525,7 +525,7 @@ body = html.Div([
                        dbc.Badge("mensuales", color="info", className="mr-1")]), 
                                        width={'size': 11,  "offset":1 })]),
        dbc.Row([        
-               dbc.Col(html.H5("(hasta abril 2021)"),
+               dbc.Col(html.H5("(hasta mayo 2021)"),
                                        width={ 'size': 3, "offset":1 }),
 
             ]),
